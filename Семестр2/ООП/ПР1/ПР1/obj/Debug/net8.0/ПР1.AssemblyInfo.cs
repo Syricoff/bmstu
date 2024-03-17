af321a9e58b15118ad0ad8878460be7114798e7e
Syricoff <syricov50@gmail.com>
2024-03-17 23:43:12 +0300

    ООП: ПР1 в процессе ОП: ТЗ

--- Семестр2/ООП/ПР1/ПР1/obj/Debug/net8.0/ПР1.AssemblyInfo.cs
+++ Семестр2/ООП/ПР1/ПР1/obj/Debug/net8.0/ПР1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ПР1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49416d704b113863ac6706fc37c94cfc144db8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbfb3bba129cae26dc96f480e85c0cb2b775acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ПР1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ПР1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
